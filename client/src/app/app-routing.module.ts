474b82f434e71ee80f6c9fa2621e7985e67166ed
Anthony Lamas <anthony21.lamas@gmail.com>
2024-09-09 14:18:20 -0700

    Login

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'inicio', component:PrincipalComponent}
+  {path: 'inicio', component:PrincipalComponent},
+  {path: 'login', component:PrincipalComponent}
+
 ];
 
 @NgModule({
