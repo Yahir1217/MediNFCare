8544089488f11cd562a2633fc5ca673ac1b4d055
Alexis Arath Alatorre Delgadillo <alexisaalatorred@gmail.com>
2024-09-09 14:05:58 -0700

    Se agregó la pagina Inicio

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component:PrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
