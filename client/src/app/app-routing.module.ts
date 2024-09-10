6db1b3e5834f4c8788f884b7cae74ec3575c6c07
Yahir Nava Gandara <xxyahir17@gmail.com>
2024-09-10 09:10:05 -0700

    menu

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './principal/principal.component';
 import { LoginComponent } from './admin/login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { PacienteComponent } from './paciente/paciente.component';
 
 const routes: Routes = [
   {path: 'inicio', component:PrincipalComponent},
   {path: 'login', component:LoginComponent},
-  {path: 'menu', component:MenuComponent}
+  {path: 'menu', component:MenuComponent},
+  {path: 'paciente', component:PacienteComponent}
 
 ];
 
