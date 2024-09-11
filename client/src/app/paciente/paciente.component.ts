227f83a613081cf50b22f2748344e6b8ead7d6b9
Yahir Nava Gandara <xxyahir17@gmail.com>
2024-09-11 14:43:55 -0700

    menu tronado

--- client/src/app/paciente/paciente.component.ts
+++ client/src/app/paciente/paciente.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './paciente.component.css'
 })
 export class PacienteComponent {
+  isMinimized: boolean = false;
 
+  toggleSidebar() {
+    this.isMinimized = !this.isMinimized;
+  }
 }
